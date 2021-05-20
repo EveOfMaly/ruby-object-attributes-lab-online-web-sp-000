305ff57825d3b655e9f95b2a5e4129b74f1508be
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-20 02:35:05 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,27 @@
+class Person 
+  
+  
+  
+  #writes the name of the person to the instant variable name
+  def name= (person_name)
+    @name = person_name
+  end
+  
+   #reads name of the person from instant variable name
+  def name
+    @name = person_name
+  end
+  
+  #writes the job of the person to the instant variable job
+  def job=(current_occupation)
+    @job = current_occupation
+  end
+  
+   #reads the job of the person to the instant variable job
+   def job
+     @job
+   end
+end
+
+john = Person.new
+p john.name
